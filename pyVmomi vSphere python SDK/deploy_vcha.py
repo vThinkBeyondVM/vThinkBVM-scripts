c5de2fbf4945e5e9999d25b861ef7cd5415fffd1
Vikas Shitole <vickyvision2020@gmail.com>
2018-01-01 00:35:13 +0530

    Update deploy_vcha.py

--- pyVmomi vSphere python SDK/deploy_vcha.py
+++ pyVmomi vSphere python SDK/deploy_vcha.py
@@ -1,3 +1,10 @@
+# Author: Vikas Shitole
+# Website: www.vThinkBeyondVM.com
+# Product: vCenter server/vCenter Server High Availability (VCHA)
+# Description: Python script to deploy vCenter server HA 
+# Reference:http://vthinkbeyondvm.com/how-to-manage-vcenter-server-ha-using-vsphere-python-sdk-pyvmomi-part-1/
+# How to setup pyVmomi environment?: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
+
 from pyVim.connect import SmartConnect
 from pyVmomi import vim
 import ssl
