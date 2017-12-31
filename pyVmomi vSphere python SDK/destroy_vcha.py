09b27309fd1eae4b39a33887abd1da4bf98df662
Vikas Shitole <vickyvision2020@gmail.com>
2018-01-01 00:32:57 +0530

    Destroy VCHA

---
+++ pyVmomi vSphere python SDK/destroy_vcha.py
@@ -0,0 +1,26 @@
+from pyVim.connect import SmartConnect
+from pyVmomi import vim
+import ssl
+#Destroy vCenter server HA
+s=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+s.verify_mode=ssl.CERT_NONE
+si= SmartConnect(host="10.161.34.35", user="Administrator@vsphere.local", pwd="VMware#23",sslContext=s)
+content=si.content
+
+#Getting VCHA configurator managed object
+vcha=si.content.failoverClusterConfigurator
+
+#Getting VCHA cluster manager
+vcha_cluster_manager=si.content.failoverClusterManager
+
+# Setting vCenter HA to "disabled" mode.
+task = vcha_cluster_manager.setClusterMode_Task("disabled")
+while(task.info.state != "success"):
+        continue
+
+#Getting VCHA cluster mode
+VCHA_mode=vcha_cluster_manager.getClusterMode()
+if (VCHA_mode == "disabled"):
+        vcha.destroyVcha_Task() #Destroing it
+else:
+        print "VCHA must be in disabled mode before destrying it"
