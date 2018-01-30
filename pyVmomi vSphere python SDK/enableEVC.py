416a498650489701c35cc9e2ca8b839f4ea5c58e
Vikas Shitole <vickyvision2020@gmail.com>
2018-01-30 15:46:17 +0530

    Update enableEVC.py

--- pyVmomi vSphere python SDK/enableEVC.py
+++ pyVmomi vSphere python SDK/enableEVC.py
@@ -2,7 +2,7 @@
 # Website: www.vThinkBeyondVM.com
 # Product: vCenter server/EVC (Enhanced Compatibility Mode)
 # Description: Script to get enbale/disable EVC on cluster
-# Reference: http://vthinkbeyondvm.com/category/vsphere-api/
+# Reference: http://vthinkbeyondvm.com/tutorial-how-to-manage-enhanced-vmotion-compatibility-evc-using-vsphere-python-sdk-pyvmomi
 # How to setup pyVmomi environment?: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
 
 from pyVim.connect import SmartConnect, Disconnect
