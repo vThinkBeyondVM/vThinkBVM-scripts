e9f5e2bd128434043ee87c452e2036321ab7e345
Vikas Shitole <vickyvision2020@gmail.com>
2018-03-20 19:21:50 +0530

    Confirm whether EVC cluster is patched or not

---
+++ pyVmomi vSphere python SDK/is_evc_cluster_patched.py
@@ -0,0 +1,123 @@
+# Author: Vikas Shitole
+# Website: www.vThinkBeyondVM.com
+# Product: vCenter server/EVC (Enhanced Compatibility Mode): Applicable to vSphere 6.0 and 6.5 (Not applicable for vSphere 5.5 EVC clusters)
+# Description: Script to check whether EVC cluster and its hosts are patched or not : vCenter/ESXi patches for Spectre vulnerability.
+# Reference: http://vthinkbeyondvm.com/category/vsphere-api/
+# How to setup pyVmomi environment?: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
+
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
+import atexit
+import ssl
+import sys
+import argparse
+import getpass
+
+def get_args():
+    """ Get arguments from CLI """
+    parser = argparse.ArgumentParser(
+        description='Arguments for talking to vCenter')
+
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSpehre service to connect to')
+
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='Username to use')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use')
+
+    parser.add_argument('-c', '--cluster',
+                        required=True,
+                        action='store',
+                        default=None,
+                        help='Name of the cluster you wish to check')
+
+    args = parser.parse_args()
+if not args.password:
+        args.password = getpass.getpass(
+            prompt='Enter vCenter password:')
+
+    return args
+
+
+
+args = get_args()
+#Script to get Max EVC Mode supported on all the hosts in the cluster
+s=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+s.verify_mode=ssl.CERT_NONE
+si= SmartConnect(host=args.host, user=args.user, pwd=args.password,sslContext=s)
+content=si.content
+cluster_name=args.cluster
+
+# Below method helps us to get MOR of the object (vim type) that we passed.
+def get_obj(content, vimtype, name):
+        obj = None
+        container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+        for c in container.view:
+                if name:
+                        if c.name == name:
+                                obj = c
+                                break
+                        else:
+                                obj = None
+
+        return obj
+
+#Cluster object
+cluster = get_obj(content,[vim.ClusterComputeResource],cluster_name)
+if(not cluster):
+        print "Cluster not found, please enter correct EVC cluster name"
+        quit()
+
+print "Cluster Name:"+cluster.name
+evc_cluster_manager=cluster.EvcManager()
+
+
+evc_state=evc_cluster_manager.evcState
+current_evcmode_key= evc_state.currentEVCModeKey
+
+if(current_evcmode_key):
+        print "Current EVC Mode::"+current_evcmode_key
+else:
+        print "EVC is NOT enabled on the cluster, please enable it first"
+        quit()
+
+feature_capabilities = evc_state.featureCapability
+
+flag=False
+for capability in feature_capabilities:
+
+
+        if(capability.key=="cpuid.STIBP" and capability.value=="1"):
+                print "Found new cpuid::"+ capability.key
+                flag=True
+
+        if(capability.key=="cpuid.IBPB" and capability.value=="1"):
+                print "Found new cpuid::"+ capability.key
+                flag=True
+
+        if(capability.key=="cpuid.IBRS" and capability.value=="1"):
+                print "Found new cpuid::"+ capability.key
+                flag=True
+
+
+if(not flag):
+        print "No new cpubit found on EVC cluster,hence cluster is NOT fully patched/upgraded"
+else:
+        print "EVC cluster is patched, enjoy!, this also confirms all the hosts inside this EVC cluster are patched as well"
+
+
+atexit.register(Disconnect, si)
