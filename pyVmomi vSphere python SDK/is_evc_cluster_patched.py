ebdff30ff571bafab5d690106d9287a2950da5e0
Vikas Shitole <vickyvision2020@gmail.com>
2018-03-21 20:33:34 +0530

    Added VMware KB as reference

--- pyVmomi vSphere python SDK/is_evc_cluster_patched.py
+++ pyVmomi vSphere python SDK/is_evc_cluster_patched.py
@@ -1,8 +1,7 @@
 # Author: Vikas Shitole
-# Website: www.vThinkBeyondVM.com
 # Product: vCenter server/EVC (Enhanced Compatibility Mode): Applicable to vSphere 6.0 and 6.5 (Not applicable for vSphere 5.5 EVC clusters)
 # Description: Script to check whether EVC cluster and its hosts are patched or not : vCenter/ESXi patches for Spectre vulnerability.
-# Reference: http://vthinkbeyondvm.com/category/vsphere-api/
+# Reference: https://kb.vmware.com/s/article/52085
 # How to setup pyVmomi environment?: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
 
 from pyVim.connect import SmartConnect, Disconnect
