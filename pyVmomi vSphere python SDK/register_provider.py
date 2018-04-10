8d5c7820a141227707b2656dae65f357391f294a
Vikas Shitole <vickyvision2020@gmail.com>
2018-04-10 11:12:11 +0530

    Added blog post reference

--- pyVmomi vSphere python SDK/register_provider.py
+++ pyVmomi vSphere python SDK/register_provider.py
@@ -2,7 +2,7 @@
 # Website: www.vThinkBeyondVM.com
 # Product: vCenter server/Proactive HA
 # Description: #Script to register a health update provider and adding monitored entities
-# Reference: http://vthinkbeyondvm.com/category/vsphere-api/
+# Reference: http://vthinkbeyondvm.com/tutorial-part-1-configure-manage-proactive-ha-using-pyvmomi/
 # How to setup pyVmomi environment?: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
 
 from pyVim.connect import SmartConnect, Disconnect
