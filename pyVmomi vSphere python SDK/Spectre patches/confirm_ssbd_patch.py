8a071eb21797ac0b75089c9028f874c32f402da6
Vikas Shitole <vickyvision2020@gmail.com>
2018-06-30 08:54:51 +0530

    Update confirm_ssbd_patch.py

--- pyVmomi vSphere python SDK/Spectre patches/confirm_ssbd_patch.py
+++ pyVmomi vSphere python SDK/Spectre patches/confirm_ssbd_patch.py
@@ -1,10 +1,12 @@
 # Author: Vikas Shitole
 # Product: vCenter server
 # Description: Script to confirm whether vCenter server, hypervisor and microcode patches are applied or not : vCenter/ESXi patches for SSBD vulnerability.
-# Reference: https://kb.vmware.com/s/article/55111
+# Reference: 
+# 1. https://kb.vmware.com/s/article/55111 
+# 2. http://vthinkbeyondvm.com/pyvmomi-script-to-confirm-speculative-store-bypass-disable-ssbd-mitigation-on-vsphere-patches/
 # How to setup pyVmomi environment?: 
 # Linux: http://vthinkbeyondvm.com/how-did-i-get-started-with-the-vsphere-python-sdk-pyvmomi-on-ubuntu-distro/
-#Windows: http://vthinkbeyondvm.com/getting-started-with-pyvmomi-on-windows-supports-vsphere-6-7/
+# Windows: http://vthinkbeyondvm.com/getting-started-with-pyvmomi-on-windows-supports-vsphere-6-7/
 
 from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
@@ -80,6 +82,7 @@ cluster_name=args.cluster
 print ("-------------------------------------")
 #Check whether vCenter server is patched or not
 supported_evc_mode=si.capability.supportedEVCMode
+# It is not required check only "ivy-bridge" EVC mode, you can pass anything from "intel-penryn" onwards
 for evc_mode in supported_evc_mode:
     if(evc_mode.key == "intel-ivybridge"):
         ivy_masks=evc_mode.featureMask
