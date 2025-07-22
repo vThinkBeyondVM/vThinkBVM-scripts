c206d89a520a3fa1b94596a24e4bfba717effef0
Vikas Shitole <vickyvision2020@gmail.com>
2025-07-22 07:38:10 +0530

    Update SpectrePatchValidation.ps1
    Documentation is updated

--- Powershell-PowerCLI/SpectrePatchValidation.ps1
+++ Powershell-PowerCLI/SpectrePatchValidation.ps1
@@ -1,14 +1,45 @@
 
 <#
-.SYNOPSIS PowerCLI script:For spectre vulnerability : "How to confirm whether vCenter server, ESXi hypervisor & CPU microcode patches are applied or not?" 
-.NOTES  Author:  Vikas Shitole
-.NOTES  Site:    www.vThinkBeyondVM.com
-.NOTES Reference: http://vthinkbeyondvm.com/powercli-script-confirm-esxi-host-patched-vmware-hypervisor-patched-microcode-spectre-vulnerability/
-.NOTES Please add the vCenter server IP/credetails as per your environment
-.NOTES Relese notes: 
-  VC 6.5 U1g: https://docs.vmware.com/en/VMware-vSphere/6.5/rn/vsphere-vcenter-server-65u1g-release-notes.html
-  VC 6.0 U3e: https://docs.vmware.com/en/VMware-vSphere/6.0/rn/vsphere-vcenter-server-60u3e-release-notes.html
-  VC 5.5 U3h: https://docs.vmware.com/en/VMware-vSphere/5.5/rn/vsphere-vcenter-server-55u3h-release-notes.html
+.SYNOPSIS
+    PowerCLI script to validate whether vCenter Server, ESXi hypervisor, and CPU microcode patches for Spectre vulnerability are applied.
+
+.DESCRIPTION
+    This script connects to a specified vCenter Server, iterates through all ESXi hosts in a given cluster, and creates a temporary VM on each host. It retrieves the vmware.log file from each VM and checks for specific patterns that indicate the presence of Spectre-related microcode and hypervisor patches. The results are compiled into a CSV report indicating which hosts are patched or unpatched.
+
+.PARAMETER vCenter Server Connection
+    Update the Connect-VIServer line with your vCenter Server IP/hostname, username, and password.
+
+.PARAMETER Cluster Name
+    Set the $clusterName variable to the name of your target cluster.
+
+.PARAMETER VM Name Prefix
+    Set the $vmname variable to the prefix for temporary VMs created during the check.
+
+.PARAMETER Log Download Path
+    Set the $drive variable to the local path where vmware.log files will be downloaded.
+
+.OUTPUTS
+    A CSV report (PatchStatus.csv) is generated in the D: drive (update path as needed) listing each host and its patch status.
+
+.NOTES
+    Author:  Vikas Shitole
+    Site:    www.vThinkBeyondVM.com
+    Reference: http://vthinkbeyondvm.com/powercli-script-confirm-esxi-host-patched-vmware-hypervisor-patched-microcode-spectre-vulnerability/
+    Release Notes:
+      VC 6.5 U1g: https://docs.vmware.com/en/VMware-vSphere/6.5/rn/vsphere-vcenter-server-65u1g-release-notes.html
+      VC 6.0 U3e: https://docs.vmware.com/en/VMware-vSphere/6.0/rn/vsphere-vcenter-server-60u3e-release-notes.html
+      VC 5.5 U3h: https://docs.vmware.com/en/VMware-vSphere/5.5/rn/vsphere-vcenter-server-55u3h-release-notes.html
+
+.EXAMPLE
+    # Update the following variables as per your environment:
+    # - vCenter Server connection details
+    # - $clusterName
+    # - $vmname
+    # - $drive
+    # - Output CSV path
+    #
+    # Run the script in a PowerCLI-enabled PowerShell session.
+
 #>
 
 #vCenter Connection, please modify as per your env.
